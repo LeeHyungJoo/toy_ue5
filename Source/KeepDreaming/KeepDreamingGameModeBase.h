adf52020562ef1c636a7a180fdd161aa2fff86d1
Yodle_94 <sj_artist@naver.com>
2022-11-25 20:34:58 +0900

    Initial commit

---
+++ Source/KeepDreaming/KeepDreamingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KeepDreamingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEEPDREAMING_API AKeepDreamingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
