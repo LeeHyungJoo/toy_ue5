53bf0526ba8f04c2e17a45d5cdc7a7a8b815c148
Yodle_94 <sj_artist@naver.com>
2022-11-29 16:57:51 +0900

    LogTest
    LogTemp 가 먹히질 않아서 LogInit 카테고리를 활용. 우선, 액터에 해당 C++ 클래스 (엑터 컴포넌트) 추가해서 간단한 로그 테스트 완료

--- Source/KeepDreaming/Private/BaseUnit.cpp
+++ Source/KeepDreaming/Private/BaseUnit.cpp
@@ -9,7 +9,7 @@ UBaseUnit::UBaseUnit()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
+	UE_LOG(LogInit, Error, TEXT("UBaseUnit Init!"));
 	// ...
 }
 
@@ -19,7 +19,7 @@ void UBaseUnit::BeginPlay()
 {
 
 	Super::BeginPlay();
-
+	UE_LOG(LogInit, Error, TEXT("UBaseUnit BeginPlay!"));
 	// ...
 	
 }
@@ -31,5 +31,7 @@ void UBaseUnit::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+
+	UE_LOG(LogInit, Error, TEXT("U_BASEUNIT_TICK : %f"), DeltaTime);
 }
 
