4f6f8730a2792c06277c8980b90566951fd6cfa8
Yodle_94 <sj_artist@naver.com>
2022-12-06 18:21:15 +0900

    Log 수정.

--- Source/KeepDreaming/KeepDreaming.h
+++ Source/KeepDreaming/KeepDreaming.h
@@ -4,7 +4,7 @@
 
 #include "EngineMinimal.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(HJ, Log, All);
+DECLARE_LOG_CATEGORY_CLASS(HJ, Log, All);
 #define _HJ_INFO (FString(__FUNCTION__) + TEXT("- [") + FString::FromInt(__LINE__)+ TEXT("]"))
-#define HJ_CALL_LOG (Verbosity) UE_LOG(HJ, Verbosity, TEXT("%s"), *_HJ_INFO)
-#define HJ_LOG (Verbosity,Format, ...) UE_LOG(HJ, Verbosity, TEXT("%s - %s"), *_HJ_INFO, *FString::Printf(Format, ##__va_args__))
+#define HJ_CALL_LOG(Verbosity) UE_LOG(HJ, Verbosity, TEXT("%s"), *_HJ_INFO)
+#define HJ_LOG(Verbosity,Format, ...) UE_LOG(HJ, Verbosity, TEXT("%s - %s"), *_HJ_INFO, *FString::Printf(Format, ##__VA_ARGS__))
